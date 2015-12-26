11358fb647f2dc7407097c9b889dc51f1d5ac8ba
Peter Ajtai <peter.ajtai@thinksolid.com>
2015-12-26 09:54:24 -0800

    Subscripions roughly working

--- index.js
+++ index.js
@@ -1,50 +1,99 @@
 'use strict';
 
+var _ = require('lodash');
+
 module.exports = {
     init : init
 };
 
+/**
+ * Creates a new application state and exposes the methods to modify and track it.
+ * @returns {stateMethods}
+ */
 function init() {
-    var data = {
-            data : {
 
-            }
+    var state = {
+            data : {},
+            subscribers : {}
         },
-        subscriptions = [];
+        /**
+         * @typedef {object} stateMethods
+         */
+        methods = {
+            get         : get.bind(state),
+            set         : set.bind(state),
+            subscribe   : subscribe.bind(state)
+        };
 
-    return {
-        get         : get.bind(null, data),
-        set         : set.bind(null, data),
-        subscribe   : subscribe.bind(null, subscriptions)
-    }
+    methods.subscribe.subscribers = subscribers.bind(state);
+
+    return methods;
 }
 
-function subscribe() {
+/**
+ * Subscribe to a path. You will be notified of change that can potentially affect your
+ * subscription.
+ * @param path {string}
+ * @returns {number}
+ */
 
+/**
+ *
+ * @param path {string}
+ * @param subscriber {object}
+ * @returns {number}
+ */
+function subscribe(path, subscriber) {
+
+    this.subscribers[path] = this.subscribers[path] || [];
+    this.subscribers[path].push(subscriber);
+    return this.subscribers[path].length;
 }
 
-function get(obj, desc) {
-    var arr;
+/**
+ *
+ * @param path {string}
+ * @returns {number}
+ */
+function subscribers(path) {
+    return undefined === this.subscribers[path] ? 0 : this.subscribers[path].length;
+}
 
-    if (!desc) {
-        return obj;
+/**
+ *
+ * @param path {string}
+ * @returns {*}
+ */
+function get(path) {
+    var arr,
+        data = this.data;
+
+    if (!path) {
+        return data;
     }
 
-    arr = desc.split('.');
-    while(arr.length && obj) {
+    arr = path.split('.');
+    while(arr.length && data) {
         // will work with arrays
-        obj = obj[arr.shift()];
+        data = data[arr.shift()];
     }
 
-    return obj;
+    return data;
 }
 
-function set(data, desc, value) {
+/**
+ *
+ * @param path
+ * @param value
+ * @returns {object}
+ */
+function set(path, value) {
     var arr,
         item,
-        obj = data;
+        originalPath = path,
+        obj = this.data;
 
-    arr = desc.split('.');
+    arr = path.split('.');
     while(arr.length > 1) {
         item = arr.shift();
         if (!obj[item]) {
@@ -55,6 +104,39 @@ function set(data, desc, value) {
     }
 
     obj[arr.shift()] = value;
+    notifySubscribers.call(this, originalPath);
 
-    return data;
+    return this.data;
+}
+
+function notifySubscribers(changedPath) {
+
+    var self = this;
+
+    _.forEach(this.subscribers, function(subscribers, subscriptionPath) {
+
+        var startsWith = new RegExp('^' + subscriptionPath),
+            contains = new RegExp('^' + changedPath);
+
+        // Notify if sub path is included in changed path
+        if (startsWith.test(changedPath)) {
+            _.forEach(subscribers, function(subscriber) {
+                subscriber.call(self, changedPath);
+            });
+
+        // Notify if exact match
+        } else if (subscriptionPath === changedPath) {
+            _.forEach(subscribers, function(subscriber) {
+                subscriber.call(self, changedPath);
+            });
+
+        // Notify if set path is included in subscription path
+        } else if (contains.test(subscriptionPath)) {
+            _.forEach(subscribers, function(subscriber) {
+                subscriber.call(self, changedPath);
+            });
+        }
+    });
+
+
 }
--- test/indexTest.js
+++ test/indexTest.js
@@ -1,8 +1,11 @@
 'use strict';
 
-var appState = require('../index');
+var appState = require('../index'),
+    chai = require('chai'),
+    spies = require('chai-spies');
 
-require('chai').should();
+chai.should();
+chai.use(spies);
 
 describe('app state', function() {
     describe('set', function() {
@@ -23,6 +26,54 @@ describe('app state', function() {
 
             state.set('data.a.b', { c : 4 });
             state.get('data.a.b.c').should.equal(4);
-        })
+        });
+    });
+    describe('subscribe', function() {
+        it('subscriber count is initially zero for a path', function() {
+            var state = appState.init();
+
+            state.subscribe.subscribers('a.b.c').should.equal(0);
+        });
+        it('subscriber count returns number of subscribers for exact path', function() {
+            var state = appState.init();
+
+            state.subscribe('a.b', function() {});
+            state.subscribe('a.b', function() {});
+            state.subscribe('a.b', function() {});
+
+            state.subscribe.subscribers('a.b').should.equal(3);
+        });
+        describe('subscription notification', function() {
+            it('will notify subscribers on exact path that is set', function() {
+                var state = appState.init(),
+                    spy = chai.spy();
+
+                state.subscribe('a.b', spy);
+
+                spy.should.have.been.called.exactly(0);
+                state.set('a.b', 2);
+                spy.should.have.been.called.exactly(1);
+            });
+            it('will notify subscribers on path that is included in set path', function() {
+                var state = appState.init(),
+                    spy = chai.spy();
+
+                state.subscribe('user', spy);
+                spy.should.have.been.called.exactly(0);
+                state.set('user.identity.name', 'Clara');
+                spy.should.have.been.called.exactly(1);
+
+            });
+            it('will notify subscribers on path that is an extension of set path', function() {
+                var state = appState.init(),
+                    spy = chai.spy();
+
+                state.subscribe('user.identity.authorized', spy);
+                spy.should.have.been.called.exactly(0);
+                state.set('user.identity', null);
+                spy.should.have.been.called.exactly(1);
+            });
+            // TODO: check things like 'userName.first' does not notify 'user'
+        });
     });
 });
